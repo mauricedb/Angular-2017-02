d20d80243ef9cb53dc10df89e53708f9edef16e8
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-03-15 11:25:40 +0100

    Before demo

---
+++ mod-04/zones-demo/index.js
@@ -0,0 +1,23 @@
+function main() {
+    b1.addEventListener('click', bindSecondButton);
+}
+
+function bindSecondButton() {
+    b2.addEventListener('click', () => setTimeout(throwError, 500));
+}
+
+
+function throwError() {
+    throw new Error('aw shucks');
+}
+
+main();
+
+// Zone.current.fork({
+//         name: 'error-logger',
+//         onHandleError: function (parentZoneDelegate, currentZone, targetZone, error) {
+//             console.log(error.stack);
+//         }
+//     })
+//     .fork(Zone.longStackTraceZoneSpec)
+//     .run(main);
