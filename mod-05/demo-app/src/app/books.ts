dec72777f65fb5a31f86ce2ecc236fe1c15ddbd8
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-03-29 11:09:58 +0200

    Before demo

---
+++ mod-05/demo-app/src/app/books.ts
@@ -0,0 +1,32 @@
+export default [{
+    id: 1,
+    title: "The Ultimate Hitchhiker's Guide to the Galaxy",
+    author: "Douglas Adams",
+    year: 1985,
+    image: "/assets/images/TheUltimateHitchhikersGuide.jpg",
+}, {
+    id: 2,
+    title: "2001: A Space Odyssey",
+    author: "Arthur C. Clarke",
+    year: 1968,
+    image: "/assets/images/2001ASpaceOdyssey.jpg",
+}, {
+    id: 3,
+    title: "The War of the Worlds",
+    author: "H G Wells",
+    year: 1898,
+    image: "/assets/images/TheWaroftheWorlds.jpg",
+}, {
+    id: 4,
+    title: "Jurassic Park",
+    author: "Michael Crichton",
+    year: 1993,
+    image: "/assets/images/JurassicPark.jpg",
+}, {
+    id: 5,
+    title: "I, Robot",
+    author: "Isaac Asimov",
+    year: 2004,
+    image: "/assets/images/IRobot.jpg",
+}];
+
